71a5afbe1b5538b167e84c5f12ad3797572ee94c
Michael Hassett <mhassett@iinet.net.au>
2023-11-25 15:27:23 +1100

    Added an angle correction to keep between 0 and 360 Added placing particles in real space. Added plotting the real space with all particles

--- calamitic.py
+++ calamitic.py
@@ -5,6 +5,8 @@ Edited by Michael Hassett from 2023-11-23
 """
 
 import numpy as np
+from PIL import Image
+from PIL import ImageDraw
 import matplotlib.pyplot as plt
 from pyFAI.azimuthalIntegrator import AzimuthalIntegrator
 
@@ -37,7 +39,7 @@ class PointParticle:
 
 
 class CalamiticParticle(PointParticle):
-    def __init__(self, position, width, length, angle):
+    def __init__(self, init_position, width, length, angle):
         """
         A calamitic (rod-like) particle in real space
         :param position: Position of the particle in Cartesian coordinates
@@ -45,11 +47,12 @@ class CalamiticParticle(PointParticle):
         :param length: Length of the particle
         :param angle: Angle of the particle in real space
         """
-        super().__init__(position)
+        super().__init__(init_position)
         self.size = (width, length)  # Width and length of the particle
         self.__set_angle__(angle)
+        self.end_position = self.get_end_points()
 
-    def __get_wid__(self):
+    def get_width(self):
         return self.size[0]
 
     def __get_len__(self):
@@ -63,6 +66,13 @@ class CalamiticParticle(PointParticle):
 
     def __set_angle__(self, input_angle):
         self.angle = input_angle
+
+    def get_end_points(self):
+        angle_rad = np.deg2rad(self.angle)
+        x1, y1 = self.position
+        x2, y2 = [int(x1 + self.__get_len__() * np.cos(angle_rad)),
+                  int(y1 + self.__get_len__() * np.sin(angle_rad))]
+        return x2, y2
 
 
 def generate_positions(x_change, y_change):
@@ -84,7 +94,7 @@ def generate_positions(x_change, y_change):
             x_pos += np.random.randint(-x_change, x_change)
         if y_change != 0:
             y_pos += np.random.randint(-y_change, y_change)
-        yield [x_pos, y_pos]
+        yield x_pos, y_pos
         x += x_spacing
 
         # When the position is at the edge of the box, adjust y and reset x
@@ -100,42 +110,62 @@ def generate_angles(mean_angle: int, angle_stddev: int):
     :param angle_stddev: standard deviation for the angle
     :return:
     """
-    angle = mean_angle
     while True:
-        angle = np.random.normal(mean_angle, angle_stddev)
-        if angle >= 360:
-            angle -= 360
-        if angle < 0:
-            angle += 360
+        angle = angle_correction(np.random.normal(mean_angle, angle_stddev))
         yield angle
+
+
+def angle_correction(angle):
+    while angle > 360:
+        angle -= 360
+    while angle < 0:
+        angle += 360
+    return angle
 
 
 if __name__ == "__main__":
     # Initialise real space grid
     x_max = y_max = 1000
     grid_size = (x_max, y_max)
-    real_space = np.zeros(grid_size)
+    real_space = Image.new('L', grid_size, 0)
+    add_to_real_space = ImageDraw.Draw(real_space)
 
     # Initialise single particle parameters
     particle_width = 2
     particle_length = 15
     unit_vector = 90  # unit vector of the particle, starting point up
-    vector_range = 45  # Full angular range for the unit vector
+    vector_range = 40  # Full angular range for the unit vector
     vector_min, vector_max = (unit_vector + change for change in (-vector_range / 2, vector_range / 2))
     unit_vector = np.random.randint(vector_min, vector_max)
+    # print(f'Min. Angle: {vector_min}\N{DEGREE SIGN}, Max. Angle: {vector_max}\N{DEGREE SIGN}')
     print(f'Unit Vector: {unit_vector}\N{DEGREE SIGN}')
     # Note: The unit vector is not the exact angle all the particles will have, but the mean of all the angles
 
     # Initialise how the particles sit in real space
-    standard_spacing = 3
-    x_spacing = particle_width + standard_spacing
-    y_spacing = int(np.ceil(particle_length * np.cos(np.deg2rad(unit_vector - 90)))) + standard_spacing
+    unit_vector_radians = np.deg2rad(unit_vector)
+    standard_spacing = 5
+    # Allow spacing in x and y to account for the size and angle of the particle
+    x_spacing = standard_spacing + (int(abs(np.round(particle_length * np.cos(unit_vector_radians)))) +
+                                    int(abs(np.round(particle_width * np.sin(unit_vector_radians)))))
+    y_spacing = standard_spacing + (int(abs(np.round(particle_length * np.sin(unit_vector_radians)))) +
+                                    int(abs(np.round(particle_width * np.cos(unit_vector_radians)))))
     print(f'x spacing: {x_spacing}, y spacing: {y_spacing}')
 
     # Generate the particles
-    # particles = CalamiticParticle(0, 0, particle_width, particle_length, unit_vector)
     positions = generate_positions(2, 2)
     angles = generate_angles(unit_vector, 5)
     particles = [CalamiticParticle(position, particle_width, particle_length, angle)
                  for position, angle in zip(positions, angles)]
     print(f'No. of Particles: {len(particles)}')
+    for particle in particles:
+        #print(f'Start: {particle.position}, End: {particle.end_position}')
+        add_to_real_space.line([particle.position, particle.end_position], fill=1, width=particle.get_width())
+    real_space = np.asarray(real_space)
+
+    # Plot real_space
+    plt.figure(figsize=(8, 8))
+    plt.imshow(real_space, extent=[0, x_max, 0, y_max])
+    plt.title(f'Liquid Crystal Phase of Calamitic Liquid crystals, with unit vector {unit_vector}$^\circ$')
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.show()
