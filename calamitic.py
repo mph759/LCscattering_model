338596c615594f954397e490b229259ba212ea48
Michael Hassett <mhassett@iinet.net.au>
2023-11-26 14:04:03 +1100

    Modified how the spacing is calculated, to allow for different padding and wobble in x and y

--- calamitic.py
+++ calamitic.py
@@ -114,7 +114,7 @@ class CalamiticParticle(PointParticle):
         return draw_object.line([self.position, self.end_position], fill=1, width=self.get_width())
 
 
-def generate_positions(x_change, y_change):
+def generate_positions(change):
     """
     Generate a position inside cartesian coordinates, given a rough lattice with random spacial oscillations
     :param x_change: Maximum change in x
@@ -122,6 +122,8 @@ def generate_positions(x_change, y_change):
     :return: A position in Cartesian coordinates inside the grid
     """
     # Initial positions, just inside the box
+    x_change = change[0]
+    y_change = change[1]
     x = int(x_spacing / 2)
     y = int(y_spacing / 2)
 
@@ -263,29 +265,36 @@ if __name__ == "__main__":
     # Initialise particle parameters
     particle_width = 2
     particle_length = 15
-    unit_vector = 90  # unit vector of the particle, starting point up
-    vector_range = 40  # Full angular range for the unit vector
+    unit_vector = 90  # Unit vector of the particles, starting point up
+    vector_range = 40  # Full angular range for the unit vector to be randomised in
     vector_stddev = 5
 
     # Initialise how the particles sit in real space
-    standard_spacing = 6
-    wobble_allowance = np.floor((standard_spacing - 1) / 2)
+    padding_spacing = (5, 5)
+
+    # Initialise beam and detector parameters
+    wavelength = 1e-10
+    pixel_size = 5e-5
+    npt = 2000
 
     # Randomise unit_vector within given range
-    vector_min, vector_max = (unit_vector + change for change in (-vector_range / 2, vector_range / 2))
-    #unit_vector = np.random.randint(vector_min, vector_max) % 360
-    # print(f'Min. Angle: {vector_min}\N{DEGREE SIGN}, Max. Angle: {vector_max}\N{DEGREE SIGN}')
+    if vector_range != 0:
+        vector_min, vector_max = (unit_vector + change for change in (-vector_range / 2, vector_range / 2))
+        unit_vector = np.random.randint(vector_min, vector_max) % 360   # Randomise unit vector in given range
+        # print(f'Min. Angle: {vector_min}\N{DEGREE SIGN}, Max. Angle: {vector_max}\N{DEGREE SIGN}')
     print(f'Unit Vector: {unit_vector}\N{DEGREE SIGN}')
     # Note: The unit vector is not the exact angle all the particles will have, but the mean of all the angles
 
     # Allow spacing in x and y to account for the size and angle of the particle
-    x_spacing, y_spacing = pythagorean_sides(particle_length, particle_width, unit_vector)
-    x_spacing += standard_spacing
-    y_spacing += standard_spacing
+    x_spacing, y_spacing = (spacing + padding
+                            for spacing, padding
+                            in zip(pythagorean_sides(particle_length, particle_width, unit_vector), padding_spacing))
     print(f'x spacing: {x_spacing}, y spacing: {y_spacing}')
 
     # Generate the particles
-    positions = generate_positions(wobble_allowance, wobble_allowance)
+    wobble_allowance = [np.floor((spacing - 1) / 2) for spacing in padding_spacing]
+    print(wobble_allowance)
+    positions = generate_positions(wobble_allowance)
     angles = generate_angles(unit_vector, vector_stddev)
     particles = [CalamiticParticle(position, particle_width, particle_length, angle)
                  for position, angle in zip(positions, angles)]
@@ -300,14 +309,11 @@ if __name__ == "__main__":
     real_space.plot(real_space_title)
 
     tic = time.perf_counter()
-    wavelength = 1e-10
-    pixel_size = 5e-5
-    npt = 2000
     diffraction_pattern_of_real_space = DiffractionPattern(real_space, wavelength, pixel_size, npt)
-    diffraction_pattern_title = f'Diffraction pattern of Liquid Crystal Phase of Calamitic Particles'
+    diffraction_pattern_title = f'2D Diffraction pattern of Liquid Crystal Phase of Calamitic Particles'
     diffraction_pattern_of_real_space.plot_2d(diffraction_pattern_title, clim=1e7)
-    diff_1D_title = f'1D Radial Integration'
+    diff_1D_title = f'1D Diffraction pattern of Liquid Crystal Phase of Calamitic Particles'
     diffraction_pattern_of_real_space.plot_1d(diff_1D_title)
     toc = time.perf_counter()
-    print(f'Generating the diffraction pattern took {toc - tic:0.4f} seconds')
+    print(f'Generating the diffraction patterns took {toc - tic:0.4f} seconds')
     plt.show()
